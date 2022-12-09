9447de32e0eab722af80dc345733dc8b0c958a43
chamabayroune <bayrounec640@gmail.com>
2022-12-09 22:58:41 +0100

    final project

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// remove items from cart 
+
+function removeCartItems(event) {
+    var buttonClicked = event.target;
+    buttonClicked.paretelemnt.remove();
+}
+
+// update Total
+function cartContent(){
+    var cartContent = document.getElementsByClassName('cart-content')[0]
+    var cartBoxes = cartContent.getElementsByClassName()
+}
+
+
+
